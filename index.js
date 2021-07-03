3e5502cd7299a0809901b1f1707d888d246a0104
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-03 14:02:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const fs = require("fs");
+const { Collection, Client } = require("discord.js");
+const client = new Client();
+client.commands = new Collection();
+client.queue = new Map();
+
+
+fs.readdir(__dirname + "/events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach((file) => {
+        const event = require(__dirname + `/events/${file}`);
+        let eventName = file.split(".")[0];
+        client.on(eventName, event.bind(null, client));
+        console.log("Loading Event: " + eventName)
+    });
+});
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+        console.log("Loading Command: " + commandName)
+    });
+});
+
+client.config = {
+    prefix: process.env.PREFIX
+}
+
+client.login(process.env.TOKEN)
